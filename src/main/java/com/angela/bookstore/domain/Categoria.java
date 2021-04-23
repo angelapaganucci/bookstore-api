7d3fb7845e417b9f140fa63a1e8e160c2c025240
angelapaganucci <angelapaganucci@hotmail.com>
2021-04-23 13:18:12 -0300

    finalizando as classes de dominio

--- src/main/java/com/angela/bookstore/domain/Categoria.java
+++ src/main/java/com/angela/bookstore/domain/Categoria.java
@@ -1,10 +1,24 @@
 package com.angela.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Categoria {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String descricao;
