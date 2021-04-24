5c59cc6d2b931d1cdda851aa13a7cc0b18762d14
angelapaganucci <angelapaganucci@hotmail.com>
2021-04-24 00:03:11 -0300

    excecao personalizada para objeto nao encontrado

--- src/main/java/com/angela/bookstore/service/CategoriaService.java
+++ src/main/java/com/angela/bookstore/service/CategoriaService.java
@@ -7,19 +7,19 @@ import org.springframework.stereotype.Service;
 
 import com.angela.bookstore.domain.Categoria;
 import com.angela.bookstore.repositories.CategoriaRepository;
+import com.angela.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
-		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+
 	}
-	
-	
 
 }
