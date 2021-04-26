e715b678ee91d8d9c6a79d60087a3fae95fd33bd
angelapaganucci <angelapaganucci@hotmail.com>
2021-04-25 21:32:37 -0300

    Listando todos os livros por categoria

--- src/main/java/com/angela/bookstore/resources/LivroResource.java
+++ src/main/java/com/angela/bookstore/resources/LivroResource.java
@@ -1,13 +1,19 @@
 package com.angela.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.angela.bookstore.domain.Livro;
+import com.angela.bookstore.dtos.LivroDTO;
 import com.angela.bookstore.service.LivroService;
 
 @RestController
@@ -20,8 +26,15 @@ public class LivroResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Livro> findById(@PathVariable Integer id){
 		Livro obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
-		
+		return ResponseEntity.ok().body(obj);		
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat){
+		List<Livro> list = service.findAll(id_cat);
+		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);		
+	
 	}
 
 }
